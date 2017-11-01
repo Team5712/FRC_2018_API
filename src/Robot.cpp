d0fc15e7b475bab014726f24f45c065f13b7acdc
robotics <robotics@HHS-13-22.hemlock.net.local>
2017-11-01 17:00:13 -0400

    Change the C++ class organization to match modern programs

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -2,68 +2,80 @@
 #include <memory>
 #include <string>
 
-#include <IterativeRobot.h>
-#include <LiveWindow/LiveWindow.h>
-#include <SmartDashboard/SendableChooser.h>
-#include <SmartDashboard/SmartDashboard.h>
+#include "Robot.h"
 
-class Robot: public frc::IterativeRobot {
-public:
-	void RobotInit() {
-		chooser.AddDefault(autoNameDefault, autoNameDefault);
-		chooser.AddObject(autoNameCustom, autoNameCustom);
-		frc::SmartDashboard::PutData("Auto Modes", &chooser);
-	}
+Robot::Robot()
+{
 
-	/*
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * GetString line to get the auto name from the text box below the Gyro.
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * if-else structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
-	void AutonomousInit() override {
-		autoSelected = chooser.GetSelected();
-		// std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
-		std::cout << "Auto selected: " << autoSelected << std::endl;
+}
 
-		if (autoSelected == autoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			// Default Auto goes here
-		}
-	}
+Robot::~Robot()
+{
 
-	void AutonomousPeriodic() {
-		if (autoSelected == autoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			// Default Auto goes here
-		}
-	}
+}
 
-	void TeleopInit() {
+void Robot::RobotInit()
+{
 
-	}
+}
 
-	void TeleopPeriodic() {
+void Robot::AutonomousInit()
+{
 
-	}
+}
 
-	void TestPeriodic() {
-		lw->Run();
-	}
+void Robot::AutonomousPeriodic()
+{
 
-private:
-	frc::LiveWindow* lw = LiveWindow::GetInstance();
-	frc::SendableChooser<std::string> chooser;
-	const std::string autoNameDefault = "Default";
-	const std::string autoNameCustom = "My Auto";
-	std::string autoSelected;
-};
+}
+
+void Robot::TeleopInit()
+{
+
+}
+
+void Robot::TeleopPeriodic()
+{
+
+}
+
+void Robot::TestPeriodic()
+{
+
+}
+
+
+// OLD FORMATTING:
+//class Robot: public frc::IterativeRobot {
+//public:
+//	void RobotInit() {
+//
+//	}
+//
+//	void AutonomousInit() override {
+//
+//	}
+//
+//	void AutonomousPeriodic() {
+//
+//	}
+//
+//	void TeleopInit() {
+//
+//	}
+//
+//	void TeleopPeriodic() {
+//
+//	}
+//
+//	void TestPeriodic() {
+//
+//	}
+//
+//private:
+//
+//
+//
+//};
 
 START_ROBOT_CLASS(Robot)
---
+++ src/Robot.h
@@ -0,0 +1,32 @@
+/*
+ * Robot.h
+ *
+ */
+
+#ifndef SRC_ROBOT_H_
+#define SRC_ROBOT_H_
+
+#include <IterativeRobot.h>
+#include <LiveWindow/LiveWindow.h>
+
+class Robot: public frc::IterativeRobot
+{
+public:
+	Robot();
+	~Robot();
+
+	void RobotInit();
+	void AutonomousInit();
+	void AutonomousPeriodic();
+	void TeleopInit();
+	void TeleopPeriodic();
+	void TestPeriodic();
+
+private:
+	frc::LiveWindow* lw = LiveWindow::GetInstance();
+
+};
+
+
+
+#endif /* SRC_ROBOT_H_ */
