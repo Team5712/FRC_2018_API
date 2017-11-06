339e1a3f95a586498b2e5729dd7d0482875f37d6
robotics <robotics@HHS-13-22.hemlock.net.local>
2017-11-06 18:52:57 -0500

    Create PortMapping header file to designate the port mappings of devices

---
+++ src/PortMapping.h
@@ -0,0 +1,25 @@
+/*
+ * PortMapping.h
+ *
+ */
+
+#ifndef SRC_PORTMAPPING_H_
+#define SRC_PORTMAPPING_H_
+
+/**
+ * Class that will define all (or the majority of) the ports for the
+ * different devices on the Robot. This allows for easy access to
+ * all of the ports, and will enable easy changing if devices
+ * are switched around or moved.
+ */
+class PortMapping
+{
+public:
+
+
+
+};
+
+
+
+#endif /* SRC_PORTMAPPING_H_ */
