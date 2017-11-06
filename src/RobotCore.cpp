9c2586748d727008ca0c6550c37a47ddca8982dc
robotics <robotics@HHS-13-22.hemlock.net.local>
2017-11-06 18:42:13 -0500

    Create RobotCore to manage all of the low level functions of the Robot

---
+++ src/RobotCore.cpp
@@ -0,0 +1,18 @@
+/*
+ * RobotCore.cpp
+ *
+ *  Created on: Nov 6, 2017
+ *      Author: robotics
+ */
+
+#include <RobotCore.h>
+
+RobotCore::RobotCore() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RobotCore::~RobotCore() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/RobotCore.h
@@ -0,0 +1,22 @@
+/*
+ * RobotCore.h
+ *
+ */
+
+#ifndef SRC_ROBOTCORE_H_
+#define SRC_ROBOTCORE_H_
+
+class RobotCore {
+public:
+	RobotCore();
+	virtual ~RobotCore();
+
+
+
+private:
+
+
+
+};
+
+#endif /* SRC_ROBOTCORE_H_ */
