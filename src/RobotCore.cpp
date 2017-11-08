1ab7bd64fa4a3b35286e23cfd6a74ea93a66c2d3
robotics <robotics@HHS-13-22.hemlock.net.local>
2017-11-08 12:22:04 -0500

    Begin prototyping and defining the basic method for the RobotCore

--- src/RobotCore.cpp
+++ src/RobotCore.cpp
@@ -16,7 +16,7 @@ RobotCore::RobotCore() {
 	leftMotor = new TalonSRX(PortMapping::LEFT_MOTOR_PORT);
 	rightMotor = new TalonSRX(PortMapping::RIGHT_MOTOR_PORT);
 
-	drive = new DifferentialDrive(leftMotor, rightMotor);
+	drive = new DifferentialDrive(testTalon, testTalon);
 
 	// Safety feature to prevent this call from hanging
 	drive->SetExpiration(0.1);
@@ -24,5 +24,69 @@ RobotCore::RobotCore() {
 }
 
 RobotCore::~RobotCore() {
+	// Upon de-constructing this object, disable the motors
+	drive->TankDrive(0.0, 0.0);
 }
+
+double getRightEncoderTicks()
+{
+
+	return 0.0;
+}
+double getLeftEncoderTicks()
+{
+
+	return 0.0;
+}
+
+float getVelocity()
+{
+
+	return 0.0f;
+}
+void setVelocity(float velocity)
+{
+
+}
+
+float getVoltage()
+{
+
+	return 0.0f;
+}
+void setVoltage(float voltage)
+{
+
+}
+
+double getServoAngle(int servoIndex)
+{
+
+	return 0.0;
+}
+void setServoAngle(int servoIndex, double angle)
+{
+
+}
+
+std::vector<std::vector<double>> getImage()
+{
+	std::vector<std::vector<double>> rgbComp;
+
+	// RGB components
+	rgbComp.push_back(std::vector<double>()); // R
+	rgbComp.push_back(std::vector<double>()); // G
+	rgbComp.push_back(std::vector<double>()); // B
+
+	return rgbComp;
+}
+std::vector<float> getJostickInput()
+{
+	std::vector<float> inputs;
+
+	return inputs;
+}
+
+
+
 
--- src/RobotCore.h
+++ src/RobotCore.h
@@ -6,6 +6,7 @@
 #ifndef SRC_ROBOTCORE_H_
 #define SRC_ROBOTCORE_H_
 
+#include <vector>
 #include "./ctre/phoenix/MotorControl/CAN/TalonSRX.h"
 #include <WPILib.h>
 
@@ -17,14 +18,29 @@ public:
 	RobotCore();
 	virtual ~RobotCore();
 
-
+	// BEGIN API FUNCTIONS
+	// Documentation for all of the functions can be found here:
+	// https://docs.google.com/document/d/179Ak10bY4e5jxSNROtnO94GOIJD48hIszoiiU598u3o/edit#
+	double getRightEncoderTicks();
+	double getLeftEncoderTicks();
+	float getVelocity();
+	void setVelocity(float velocity);
+	float getVoltage();
+	void setVoltage(float voltage);
+	double getServoAngle(int servoIndex);
+	void setServoAngle(int servoIndex, double angle);
+	std::vector<std::vector<double>> getImage();
+	std::vector<float> getJostickInput();
+	// END API FUNCTIONS
 
 private:
 
 	CTRE::MotorControl::CAN::TalonSRX* leftMotor;
 	CTRE::MotorControl::CAN::TalonSRX* rightMotor;
 
-	RobotDriveBase* drive;
+	Talon testTalon{1};
+	DifferentialDrive* drive;
+	DifferentialDrive differential{testTalon, testTalon};
 
 };
 
